213940cd7adf86e1012bd71060e415a18e18fe75
YumingChen <mathcym@outlook.com>
2020-08-29 16:11:24 +0800

    [yuming chen] feat: java basic grammar test

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GrammarExercise {
     public static void main(String[] args) {
         //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        String firstWordList = "apple,juice,mother,people,beautiful,apple,dog";
+        String secondWordList = "cat,baby,smile,good,apple,beautiful,Dog,nice";
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
@@ -13,6 +17,35 @@ public class GrammarExercise {
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        if(!valid(firstWordList) || !valid(secondWordList)) {
+            throw new RuntimeException("input not valid");
+        }
+
+        Set<String> m1 = Arrays.stream(firstWordList.split(",")).
+                map(String::toLowerCase).collect(Collectors.toSet());
+
+        Set<String> m2 = Arrays.stream(secondWordList.split(",")).
+                map(String::toLowerCase).collect(Collectors.toSet());
+
+        return m1.stream().filter(m2::contains).sorted().collect(Collectors.toList());
     }
+
+    private static boolean valid(String s) {
+        String[] div = s.split(",");
+        return div.length == (int) Arrays.stream(div).filter(GrammarExercise::isWord).count();
+    }
+
+    private static boolean isWord(String s) {
+        if(s == null || s.length() == 0) {
+            return false;
+        }
+        for(int i = 0; i < s.length(); ++i) {
+            if(!Character.isLetter(s.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
